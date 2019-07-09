2c23ab52fadaad8c880523124b6a95c59381a892
Mathieu Girard <mat@MacBook-Pro-de-Mathieu.local>
2019-07-08 22:07:14 -0400

    dev

--- src/components/EleveScanner.js
+++ src/components/EleveScanner.js
@@ -4,20 +4,30 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
+import Quagga from 'quagga'; 
 
 
-const useStyles = makeStyles(theme => ({
-}));
 
-export default function EleveScanner(props) {
-  const classes = useStyles();
-  const {...other } = props;
-  return (
-    <Dialog {...other }>
-      <DialogTitle>Accès élève</DialogTitle>
-      Scanner...
-    </Dialog>
-  );
+
+class EleveScanner extends React.Component {
+  
+
+
+  
+  render(props) {
+    
+    const {...other } = props;
+
+    return (
+      <Dialog {...other }>
+        <DialogTitle>Accès élève</DialogTitle>
+        Scanner...
+      </Dialog>
+    );
+  }
+  
 }
 
 
+
+export default EleveScanner;
